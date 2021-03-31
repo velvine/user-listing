927768fdd7bff9920e4a6dacb897f3657c3c3093
velvine tanui <ngeshvelvine@gmail.com>
2021-03-31 16:03:40 +0300

    fixed navbar bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">UserListing</a>
+                <a className="navbar-brand">UserListing</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">About</a>
+                        <a className="nav-link active" aria-current="page">Home</a>
+                        <a className="nav-link">About</a>
                     </div>
                 </div>
             </div>

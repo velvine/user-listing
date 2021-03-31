a880aa2bc0f42eb472d2f45cb6a3a84d8cd727af
velvine tanui <ngeshvelvine@gmail.com>
2021-03-31 16:06:59 +0300

    removed anchor tag

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand">UserListing</a>
+                <span className="navbar-brand">UserListing</span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page">Home</a>
-                        <a className="nav-link">About</a>
+                        <span className="nav-link active" aria-current="page">Home</span>
+                        <span className="nav-link">About</span>
                     </div>
                 </div>
             </div>
